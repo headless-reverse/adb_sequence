27132e7c8d5a30d702fa980b5a9f15344f3543d5
headless-reverse <scrcpyshell@gmail.com>
2025-12-11 03:09:54 +0100

    Remove unnecessary whitespace in commandexecutor.h

--- commandexecutor.h
+++ commandexecutor.h
@@ -9,24 +9,19 @@ class CommandExecutor : public QObject {
 public:
     explicit CommandExecutor(QObject *parent = nullptr);
     ~CommandExecutor();
-
     void runAdbCommand(const QStringList &args);
     void stop();
-
     void setAdbPath(const QString &path);
-
 signals:
     void outputReceived(const QString &text);
     void errorReceived(const QString &text);
     void started();
     void finished(int exitCode, QProcess::ExitStatus exitStatus);
-
 private slots:
     void readStdOut();
     void readStdErr();
     void onStarted();
     void onFinished(int exitCode, QProcess::ExitStatus exitStatus);
-
 private:
     QProcess *m_process = nullptr;
     QString m_adbPath = QStringLiteral("adb");
