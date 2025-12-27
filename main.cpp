ecb2528d0d4667275c89b8e539dc253d73418401
headless-reverse <scrcpyshell@gmail.com>
2025-12-28 00:40:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
-#include <QApplication>
 #include "mainwindow.h"
+#include "argsparser.h"
+#include <QApplication>
+#include <QDebug>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
-    MainWindow w;
+    a.setApplicationName("adb_sequence"); 
+    a.setApplicationVersion("6.0");
+    a.setOrganizationName("AdbShell");
+    ArgsParser::parse(a.arguments());
+    QString adbPath = ArgsParser::get("adb-path");
+    QString targetSerial = ArgsParser::get("device-serial");
+    MainWindow w(nullptr, adbPath, targetSerial);
     w.show();
     return a.exec();}
