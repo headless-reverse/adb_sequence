e687747e42c3c9ec41f978b7843bb5b16df67746
headless-reverse <scrcpyshell@gmail.com>
2025-12-28 23:41:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,36 @@
 #include "argsparser.h"
 #include <QApplication>
 #include <QDebug>
+#include <QSurfaceFormat>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    QSurfaceFormat format;
+    format.setVersion(3, 3); 
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    format.setDepthBufferSize(24);
+    format.setStencilBufferSize(8);
+    
+    format.setSwapBehavior(QSurfaceFormat::DoubleBuffer);
+    format.setSamples(4);
+    
+    QSurfaceFormat::setDefaultFormat(format);
+
     QApplication a(argc, argv);
+    
     a.setApplicationName("adb_sequence"); 
-    a.setApplicationVersion("6.0");
+    a.setApplicationVersion("4.0");
     a.setOrganizationName("AdbShell");
+    
     ArgsParser::parse(a.arguments());
+    
     QString adbPath = ArgsParser::get("adb-path");
     QString targetSerial = ArgsParser::get("device-serial");
+    if (adbPath.isEmpty()) {
+        qDebug() << "Warning: No adb-path provided, using default 'adb'";
+    }
+    
     MainWindow w(nullptr, adbPath, targetSerial);
     w.show();
-    return a.exec();}
+    
+    return a.exec();
+}
