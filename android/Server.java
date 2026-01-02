e15e7c3111301985f04c3ecd398bc7fd7e7660fb
headless-reverse <scrcpyshell@gmail.com>
2026-01-02 04:48:20 +0100

    Update Server.java

--- android/Server.java
+++ android/Server.java
@@ -6,44 +6,62 @@ import android.graphics.Rect;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.lang.reflect.Method;
 
 public class Server {
     public static void main(String[] args) {
         try {
-            new Server().start(7373);
+            int port = 7373;
+            if (args.length > 0) {
+                port = Integer.parseInt(args[0]);
+            }
+            new Server().start(port);
         } catch (Exception e) {
+            System.err.println("Fatal error: " + e.getMessage());
             e.printStackTrace();
             System.exit(1);
         }
     }
 
     public void start(int port) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Server started. Waiting for Qt...");
-        
-        Socket client = serverSocket.accept();
-        DataOutputStream out = new DataOutputStream(client.getOutputStream());
-
-        ScreenEncoder encoder = new ScreenEncoder(720, 1280, 4000000);
-        encoder.stream(null, out); 
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server started on port " + port + ". Waiting for connection...");
+            
+            while (true) {
+                try (Socket client = serverSocket.accept()) {
+                    client.setTcpNoDelay(true);
+                    System.out.println("Qt Client connected: " + client.getRemoteSocketAddress());
+                    DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                    int width = 720;
+                    int height = 1280;
+                    int bitrate = 4000000;
+                    ScreenEncoder encoder = new ScreenEncoder(width, height, bitrate);
+                    encoder.stream(out); 
+                } catch (Exception e) {
+                    System.err.println("Connection error: " + e.getMessage());
+                }
+                System.out.println("Ready for next connection...");
+            }
+        }
     }
 
     public static void createDisplayMirror(Surface surface, int w, int h) throws Exception {
-        IBinder display = SurfaceControl.getBuiltInDisplay();
-        if (display == null) {
-            throw new RuntimeException("Could not get display token");
-        }
+    IBinder display = SurfaceControl.getBuiltInDisplay();
 
-        IBinder virtualDisplay = SurfaceControl.createDisplay("adb_sequence", false);
-
-        SurfaceControl.openTransaction();
-        try {
-            SurfaceControl.setDisplaySurface(virtualDisplay, surface);
-            SurfaceControl.setDisplayProjection(virtualDisplay, 0, new Rect(0, 0, w, h), new Rect(0, 0, w, h));
-            SurfaceControl.setDisplayLayerStack(virtualDisplay, 0);
-        } finally {
-            SurfaceControl.closeTransaction();
-        }
-        System.out.println("adb_sequence - activated!");
+    if (display == null) {
+        throw new RuntimeException("Could not get display token. Are you running as shell/root?");
     }
-}
+
+    IBinder virtualDisplay = SurfaceControl.createDisplay("adb_sequence", false);
+
+    SurfaceControl.openTransaction();
+    try {
+        SurfaceControl.setDisplaySurface(virtualDisplay, surface);
+        Rect rect = new Rect(0, 0, w, h);
+        SurfaceControl.setDisplayProjection(virtualDisplay, 0, rect, rect);
+        SurfaceControl.setDisplayLayerStack(virtualDisplay, 0);
+    } finally {
+        SurfaceControl.closeTransaction();
+    }
+    System.out.println("adb_sequence - Virtual Display Mirror Activated!");
+}}
