fb6758f4253f14fa2364c269489007faeb7bb468
headless-reverse <scrcpyshell@gmail.com>
2026-01-04 03:31:07 +0100

    Update SurfaceControl.java

--- android/SurfaceControl.java
+++ android/SurfaceControl.java
@@ -12,44 +12,36 @@ public final class SurfaceControl {
         try {
             CLASS = Class.forName("android.view.SurfaceControl");
         } catch (ClassNotFoundException e) {
-            throw new AssertionError(e);
-        }
-    }
+            throw new AssertionError(e);}}
 
     public static void openTransaction() {
         try {
             CLASS.getMethod("openTransaction").invoke(null);
-        } catch (Exception e) { throw new AssertionError(e); }
-    }
+        } catch (Exception e) { throw new AssertionError(e); }}
 
     public static void closeTransaction() {
         try {
             CLASS.getMethod("closeTransaction").invoke(null);
-        } catch (Exception e) { throw new AssertionError(e); }
-    }
+        } catch (Exception e) { throw new AssertionError(e); }}
 
     public static void setDisplayProjection(IBinder displayToken, int orientation, Rect layerStackRect, Rect displayRect) {
         try {
             CLASS.getMethod("setDisplayProjection", IBinder.class, int.class, Rect.class, Rect.class)
                     .invoke(null, displayToken, orientation, layerStackRect, displayRect);
-        } catch (Exception e) { throw new AssertionError(e); }
-    }
+        } catch (Exception e) { throw new AssertionError(e); }}
 
     public static void setDisplayLayerStack(IBinder displayToken, int layerStack) {
         try {
             CLASS.getMethod("setDisplayLayerStack", IBinder.class, int.class).invoke(null, displayToken, layerStack);
-        } catch (Exception e) { throw new AssertionError(e); }
-    }
+        } catch (Exception e) { throw new AssertionError(e); }}
 
     public static void setDisplaySurface(IBinder displayToken, Surface surface) {
         try {
             CLASS.getMethod("setDisplaySurface", IBinder.class, Surface.class).invoke(null, displayToken, surface);
-        } catch (Exception e) { throw new AssertionError(e); }
-    }
+        } catch (Exception e) { throw new AssertionError(e); }}
 
     public static IBinder createDisplay(String name, boolean secure) throws Exception {
-        return (IBinder) CLASS.getMethod("createDisplay", String.class, boolean.class).invoke(null, name, secure);
-    }
+        return (IBinder) CLASS.getMethod("createDisplay", String.class, boolean.class).invoke(null, name, secure);}
 
     public static IBinder getBuiltInDisplay() {
         try {
@@ -63,14 +55,8 @@ public final class SurfaceControl {
                 } catch (NoSuchMethodException e) {
                     long[] ids = (long[]) CLASS.getMethod("getPhysicalDisplayIds").invoke(null);
                     if (ids != null && ids.length > 0) {
-                        return (IBinder) CLASS.getMethod("getPhysicalDisplayToken", long.class).invoke(null, ids[0]);
-                    }
-                    throw e;
-                }
-            }
+                        return (IBinder) CLASS.getMethod("getPhysicalDisplayToken", long.class).invoke(null, ids[0]);}
+                    throw e;}}
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
-        }
-    }
-}
+            return null;}}}
